f395d23c983d4b6b3bf6cd1bcb35452415de3a69
pilicercato <mpilicercato@gmail.com>
2017-08-24 18:08:48 -0300

    alta en dos columnas

--- primeraEntrega/PacienteRegistrar.php
+++ primeraEntrega/PacienteRegistrar.php
@@ -20,15 +20,19 @@
 	<p>Registrar Paciente</p>
 
 	<form>
-		Nombre: <input type="text" name="name" placeholder="Ingrese el Nombre">
+		<div style="width:50%; float:left;">
+ 		Nombre: <input type="text" name="name" placeholder="Ingrese el Nombre">
 		Apellido: <input type="text" name="lastname" placeholder="Ingrese el Apellido">
 		DNI: <input type="text" name="dni" placeholder="Ingresa el dni">
 		Obra Social: <input type="text" name="user" placeholder="Ingrese la obra social">
 		Numero de Afiliado: <input type="text" name="pass" placeholder="Ingrese el numero de afiliado">
+		</div>
+		<div style="width:50%; float:left;">
 		Email: <input type="email" name="email" placeholder="ejemplo@gmail.com">
 		Fecha de Nacimiento: <input type="date" name="nacimiento">
 		Telefono: <input type="text" name="telefono" placeholder="Ingresa el telefono">
 		Direccion: <input type="text" name="direccion" placeholder="Ingresa la direccion">
+		</div>
 		<input type="submit" name="submit">
 	</form>
 
