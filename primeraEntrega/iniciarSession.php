5354f9f9022117b7da535d8ac155b945cca12ca2
pilicercato <mpilicercato@gmail.com>
2017-08-24 16:37:33 -0300

    esto

---
+++ primeraEntrega/iniciarSession.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <link href="../css/menu.css" rel="stylesheet" type="text/css">
+    <link href="../css/header.css" rel="stylesheet" type="text/css">
+    <link href="../css/formularios.css" rel="stylesheet" type="text/css">
+    <link href="../css/footer.css" rel="stylesheet" type="text/css">
+    <head>
+        <title>
+            Iniciar Session
+        </title>
+    </head>
+    <body>
+    <header>
+    <h1>
+        El titulo de INICIO que se te cante
+    </h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="../index.html">Inicio</a></li>
+        </ul>
+    </nav>
+    <p>Iniciar Session</p>
+    <form>
+        Usuario:
+        <input name="user" placeholder="Ingrese el usuario" type="text">
+        Contraseña:
+        <input name="pass" placeholder="Ingrese la contraseña" type="text">
+        <input name="submit" type="submit" value="ingresar">
+    </form>
+    <footer>Pilis+Plomero@2017all Rights Reserved</footer>
+</body>
+</html>
