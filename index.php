405fa7b60a50c846d5e4b6ef8e73e3e16f1de8e1
FaacuuDiaz <facu_tls95@hotmail.com>
2017-09-14 16:18:37 -0300

    responsiveando

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 
 <!DOCTYPE html>
 <html>
+
+<head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no ,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/menu.css">
 	<link rel="stylesheet" type="text/css" href="css/header.css">
 	<link rel="stylesheet" type="text/css" href="css/footer.css">
 	<link rel="stylesheet" type="text/css" href="css/textoIntro.css">
-<head>
+
 	<title>Inicio</title>
 </head>
 <body>
@@ -16,58 +18,75 @@
 		<ul>
 			<li><a href="./primeraEntrega/iniciarSession.php">Iniciar Sesion</a></li>
 			<li><a href="./primeraEntrega/registrarse.php">Registrarse</a></li>
-		
+
+		<li>
 		<form id="searchform" action="primeraEntrega/busquedaPaciente.php">
 			<input type="text" placeholder="Buscar..."/>
 			<button>Ir</button>
 		</form>
+		</li>
 		</ul>
 	</nav>
+	<div class="botonResponsive">
+		<form id="searchformResponsive" action="primeraEntrega/busquedaPaciente.php">
+			<input type="text" placeholder="Buscar..."/>
+			<button>Ir</button>
+		</form>
+	</div>
 	<br><br><br><br>
 	<table class="contenido">
+	<tr>
 	<th>
-		<form action="pagina1.php" method="encritp-type">
-			<label> El Hospital </label>
+			 El Hospital
+	</th>
+	<th>
+			Guardia
+	</th>
+	<th>
+			Especialidades
+	</th>
+	</tr>
+		<tr>
+	<td>
+		<form action="pagina1.php" method="post">
 			<div class="textoIntro">
 			Este centro de salud tiene un programa de residencia de primer nivel en el pais. Se ofrecen oportunidades de practica intensiva y supervisada en ambitos profesionales por la misma -por supuesto- se recibe un salario mensual, acorde a lo que la regulacion medica profesional lo indica en cada momento
 			</div>
 			<input type="submit" name="pagina1" value="mas Info">
-			
+
 		</form>
-		</th>
-		<th>
-		<form action="pagina1.php" method="encritp-type">
-			<label>Guardia</label>
+		</td>
+		<td>
+		<form action="pagina1.php" method="post">
 			<div class="textoIntro">
 			Hospital DR. Ricardo Gutierrez de La Plata dispone de un sofisticado servicio de guardias medicas las 24 horas para la atencion de distintas urgencias. La administracion de la institucion hace viable una eficiente separacion de los pacientes segun el nivel de seriedad y tipo de patologia
 			</div>
 			<input type="submit" name="pagina1" value="mas Info">
 		</form>
-		</th>
-		<th>
-		<form action="pagina1.php" method="encritp-type">
-			<label> Especialidades</label>
+		</td>
+		<td>
+		<form action="pagina1.php" method="post">
 			<div class="textoIntro">
 			Acorde a una respetable trayectoria en materia de medicina y salud, en Hospital DR. Ricardo Gutierrez de La Plata podemos encontrar profesionales de las principales especialidades de salud. Del mismo modo, se brinda atencion programada y de urgencias, se realizan estudios medicos y se brinda soporte en muchas ramas comunes de la medicina moderna
 			</div>
 			<input type="submit" name="pagina1" value="mas Info">
 		</form>
-		</th>
-		<tr>
+		</td>
+		</tr>
 	</table>
 	<div class="contenidoLista">
 		<ul>
 		<li>
-			<form action="pagina1.php" method="encritp-type">
+			<form action="pagina1.php" method="post">
 				<label> El Hospital </label>
 				<div class="textoIntro">
 				Este centro de salud tiene un programa de residencia de primer nivel en el pais. Se ofrecen oportunidades de practica intensiva y supervisada en ambitos profesionales por la misma -por supuesto- se recibe un salario mensual, acorde a lo que la regulacion medica profesional lo indica en cada momento
 				</div>
-				<input type="submit" name="pagina1" value="mas Info">	
+				<input type="submit" name="pagina1" value="mas Info">
 			</form>
 		</li>
 		<li>
-			<form action="pagina1.php" method="encritp-type">
+			<form action="pagina1.php" method="post">
 				<label>Guardia</label>
 				<div class="textoIntro">
 				Hospital DR. Ricardo Gutierrez de La Plata dispone de un sofisticado servicio de guardias medicas las 24 horas para la atencion de distintas urgencias. La administracion de la institucion hace viable una eficiente separacion de los pacientes segun el nivel de seriedad y tipo de patologia
@@ -75,18 +94,18 @@
 				<input type="submit" name="pagina1" value="mas Info">
 			</form>
 		</li>
-		<li>	
-			<form action="pagina1.php" method="encritp-type">
+		<li>
+			<form action="pagina1.php" method="post">
 				<label> Especialidades</label>
 				<div class="textoIntro">
 				Acorde a una respetable trayectoria en materia de medicina y salud, en Hospital DR. Ricardo Gutierrez de La Plata podemos encontrar profesionales de las principales especialidades de salud. Del mismo modo, se brinda atencion programada y de urgencias, se realizan estudios medicos y se brinda soporte en muchas ramas comunes de la medicina moderna
 				</div>
 				<input type="submit" name="pagina1" value="mas Info">
 			</form>
-		</li>	
+		</li>
 	</ul>
 	</div>
 
-	<footer class="footer">Proyecto de Software 2017 - Hospital Dr. Ricardo Gutierrez</footer>
+	<footer >Proyecto de Software 2017 - Hospital Dr. Ricardo Gutierrez</footer>
 </body>
 </html>
--- primeraEntrega/iniciarSession.php
+++ primeraEntrega/iniciarSession.php
@@ -1,34 +1,36 @@
 <!DOCTYPE html>
 <html>
+    <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no ,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link href="../css/menu.css" rel="stylesheet" type="text/css">
     <link href="../css/header.css" rel="stylesheet" type="text/css">
     <link href="../css/formularios.css" rel="stylesheet" type="text/css">
     <link href="../css/footer.css" rel="stylesheet" type="text/css">
-    <head>
+
+
         <title>
             Iniciar Session
         </title>
     </head>
     <body>
-    <header>
-    <h1>
-        Hospital Gutierrez
-    </h1>
-    </header>
+<header><h1>Hospital DR. Ricardo Gutierrez</h1></header>
     <nav class="menu">
         <ul>
             <li><a href="../index.php">Inicio</a></li>
         </ul>
     </nav>
     <p>Iniciar Session</p>
-    <form>
+    <form action="./usuarioAdmin.php">
         Usuario:
-        <input name="user" placeholder="Ingrese el usuario" type="text">
+        <input name="user" pattern="[a-zA-Z]{4,15}$" placeholder="Ingrese el usuario" data-error="Ingresar usuario valido."
+        type="text" required>
         Contraseña:
-        <input name="pass" placeholder="Ingrese la contraseña" type="text">
+        <input name="pass" placeholder="Ingrese la contraseña" pattern="[a-z0-9A-Z]{6,12}$" data-error="Ingresar contraseña valida."
+        type="password" required>
         <input name="submit" type="submit" value="Aceptar">
     </form>
-    <footer class="foot">Proyecto de Software 2017 - Hospital Dr. Ricardo Gutierrez</footer>
+
+    <footer class="footer" >Proyecto de Software 2017 - Hospital Dr. Ricardo Gutierrez</footer>
 </body>
 </html>
