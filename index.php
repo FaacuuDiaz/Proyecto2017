c13895feef7266b42a910d2423fd6beaa39f2d2b
pilicercato <mpilicercato@gmail.com>
2017-09-20 15:44:41 -0300

    bootstrap

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 
+
 <!DOCTYPE html>
 <html>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no ,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/menu.css">
-	<link rel="stylesheet" type="text/css" href="css/header.css">
-	<link rel="stylesheet" type="text/css" href="css/footer.css">
-	<link rel="stylesheet" type="text/css" href="css/textoIntro.css">
 <head>
+	  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="./assets/styles/css/footer.css">
+  <link rel="stylesheet" type="text/css" href="./assets/styles/css/menu.css">
+    <link rel="stylesheet" type="text/css" href="./assets/styles/css/header.css">
+    <link rel="stylesheet" type="text/css" href="./assets/styles/css/textIntro.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 	<title>Inicio</title>
 </head>
 <body>
@@ -16,7 +21,7 @@
 		<ul>
 			<li><a href="./primeraEntrega/iniciarSession.php">Iniciar Sesion</a></li>
 			<li><a href="./primeraEntrega/registrarse.php">Registrarse</a></li>
-		
+
 		<form id="searchform" action="primeraEntrega/busquedaPaciente.php">
 			<input type="text" placeholder="Buscar..."/>
 			<button>Ir</button>
@@ -24,68 +29,35 @@
 		</ul>
 	</nav>
 	<br><br><br><br>
-	<table class="contenido">
-	<th>
+<div class="col-sm-4">
 		<form action="pagina1.php" method="encritp-type">
 			<label> El Hospital </label>
 			<div class="textoIntro">
 			Este centro de salud tiene un programa de residencia de primer nivel en el pais. Se ofrecen oportunidades de practica intensiva y supervisada en ambitos profesionales por la misma -por supuesto- se recibe un salario mensual, acorde a lo que la regulacion medica profesional lo indica en cada momento
 			</div>
-			<input type="submit" name="pagina1" value="mas Info">
-			
+			<input type="submit" class="btn btn-success" name="pagina1" value="mas Info">
+
 		</form>
-		</th>
-		<th>
+		</div>
+		<div class="col-sm-4">
 		<form action="pagina1.php" method="encritp-type">
 			<label>Guardia</label>
 			<div class="textoIntro">
 			Hospital DR. Ricardo Gutierrez de La Plata dispone de un sofisticado servicio de guardias medicas las 24 horas para la atencion de distintas urgencias. La administracion de la institucion hace viable una eficiente separacion de los pacientes segun el nivel de seriedad y tipo de patologia
 			</div>
-			<input type="submit" name="pagina1" value="mas Info">
+			<input type="submit" class="btn btn-success" name="pagina1" value="mas Info">
 		</form>
-		</th>
-		<th>
+		</div>
+		<div class="col-sm-4">
 		<form action="pagina1.php" method="encritp-type">
 			<label> Especialidades</label>
 			<div class="textoIntro">
 			Acorde a una respetable trayectoria en materia de medicina y salud, en Hospital DR. Ricardo Gutierrez de La Plata podemos encontrar profesionales de las principales especialidades de salud. Del mismo modo, se brinda atencion programada y de urgencias, se realizan estudios medicos y se brinda soporte en muchas ramas comunes de la medicina moderna
 			</div>
-			<input type="submit" name="pagina1" value="mas Info">
+			<input type="submit" class="btn btn-success" name="pagina1" value="mas Info">
 		</form>
-		</th>
-		<tr>
-	</table>
-	<div class="contenidoLista">
-		<ul>
-		<li>
-			<form action="pagina1.php" method="encritp-type">
-				<label> El Hospital </label>
-				<div class="textoIntro">
-				Este centro de salud tiene un programa de residencia de primer nivel en el pais. Se ofrecen oportunidades de practica intensiva y supervisada en ambitos profesionales por la misma -por supuesto- se recibe un salario mensual, acorde a lo que la regulacion medica profesional lo indica en cada momento
-				</div>
-				<input type="submit" name="pagina1" value="mas Info">	
-			</form>
-		</li>
-		<li>
-			<form action="pagina1.php" method="encritp-type">
-				<label>Guardia</label>
-				<div class="textoIntro">
-				Hospital DR. Ricardo Gutierrez de La Plata dispone de un sofisticado servicio de guardias medicas las 24 horas para la atencion de distintas urgencias. La administracion de la institucion hace viable una eficiente separacion de los pacientes segun el nivel de seriedad y tipo de patologia
-				</div>
-				<input type="submit" name="pagina1" value="mas Info">
-			</form>
-		</li>
-		<li>	
-			<form action="pagina1.php" method="encritp-type">
-				<label> Especialidades</label>
-				<div class="textoIntro">
-				Acorde a una respetable trayectoria en materia de medicina y salud, en Hospital DR. Ricardo Gutierrez de La Plata podemos encontrar profesionales de las principales especialidades de salud. Del mismo modo, se brinda atencion programada y de urgencias, se realizan estudios medicos y se brinda soporte en muchas ramas comunes de la medicina moderna
-				</div>
-				<input type="submit" name="pagina1" value="mas Info">
-			</form>
-		</li>	
-	</ul>
-	</div>
+		</div>
+
 
 	<footer class="footer">Proyecto de Software 2017 - Hospital Dr. Ricardo Gutierrez</footer>
 </body>
--- primeraEntrega/usuarioAdmin.php
+++ primeraEntrega/usuarioAdmin.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="../css/menu.css">
-	<link rel="stylesheet" type="text/css" href="../css/header.css">
-	<link rel="stylesheet" type="text/css" href="../css/footer.css">
-	<link rel="stylesheet" type="text/css" href="../css/botonBuscar.css">
-	<link rel="stylesheet" type="text/css" href="../css/contenido.css"></li>
+<html lang="es">
 <head>
-	<title>Inicio</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../assets/styles/css/footer.css">
+  <link rel="stylesheet" type="text/css" href="../assets/styles/css/menu.css">
+    <link rel="stylesheet" type="text/css" href="../assets/styles/css/header.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<header><h1>Hospital Dr. Ricardo Gutierrez</h1></header>
@@ -31,15 +33,23 @@
 
 				</ul>
 			</li>
-			<form id="searchform" action="primeraEntrega/busquedaPaciente.php">
+			<li>
+			<form id="searchform" class="botonBuscar.css" action="primeraEntrega/busquedaPaciente.php">
 				<input type="text" placeholder="Buscar..."/>
 				<button>Ir</button>
 			</form>
+			</li>
 		</ul>
 	</nav>
-	<div style="margin-top: 100px">
+<!-- 	<div class="botonResponsive">
+		<form id="searchformResponsive" action="primeraEntrega/busquedaPaciente.php">
+			<input type="text" placeholder="Buscar..."/>
+			<button>Ir</button>
+		</form>
+	</div>
+ -->	<div class="contenido"> <!-- style="margin-top: 100px"> -->
 		Bienvenido al sistema de gestion de pacientes y usuarios de el Hospital, ante cualquier incomveniente o duda puede revisar el FAQ's , y  en caso de no encontrar respuesta puede mandar un mail al departamento de sistemas del Hospital.
 	</div>
-	<footer class="foot">Proyecto de Software 2017 - Hospital Dr. Ricardo Gutierrez</footer>
+	<footer class="footer">Proyecto de Software 2017 - Hospital Dr. Ricardo Gutierrez</footer>
 </body>
 </html>
