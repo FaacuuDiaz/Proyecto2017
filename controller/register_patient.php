8fb5ed9d7c9b4d1fb1299cbd5ba0b2d514a6f773
pilicercato <mpilicercato@gmail.com>
2017-09-27 18:21:57 -0300

    cambio a twig

--- controller/register_patient.php
+++ controller/register_patient.php
@@ -1,19 +1,16 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', '1');	
-	
-	require_once("incluir_twig.php");
-	require_once("check_session.php");
-	require_once("../model/Repository_Patient.php");
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-	if(isset($_SESSION['rol'])){
-		$docs=Repository_Patient::get_TypeDocs();
-		$social=Repository_Patient::get_SocialWorks();
-		$template=$twig->loadTemplate('register_patient.html');
-		$template->display(array("rol_user"=>$_SESSION['rol'],"docs"=>$docs,"social"=>$social));
-	}
-	else{
-		header('Location:index.php');
-	}
+require_once "incluir_twig.php";
+require_once "check_session.php";
+require_once "../model/Repository_Patient.php";
 
-?>
+if (isset($_SESSION['rol'])) {
+    $docs     = Repository_Patient::get_TypeDocs();
+    $social   = Repository_Patient::get_SocialWorks();
+    $template = $twig->loadTemplate('register_patient.twig');
+    $template->display(array("rol_user" => $_SESSION['rol'], "docs" => $docs, "social" => $social));
+} else {
+    header('Location:index.php');
+}
