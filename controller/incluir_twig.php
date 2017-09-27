969798c8146d29ae0829d3fe73232e3fe79ee1b3
FaacuuDiaz <facu_tls95@hotmail.com>
2017-09-27 16:37:07 -0300

    borrando

--- controller/incluir_twig.php
+++
@@ -1,6 +0,0 @@
-<?php
-  require_once '../view/src/Twig/lib/Twig/Autoloader.php';
-  Twig_Autoloader::register();
-  $loader= new Twig_Loader_Filesystem('../view/templates');
-  $twig= new Twig_Environment($loader, array('cache' => false));
-?>
